174c83b9975a1b441919b2093f050279bdb35a7a
Yinkai <tonyxiong995@gmail.com>
2023-10-14 19:32:00 -0400

    Add sign in, sign up and dashboard page.

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,7 +1,9 @@
 import { Paths } from './constants/Paths.js';
 import { Home } from './pages/Home/home.jsx';
 import { Layout } from './pages/Home/layout.jsx';
+import { SignUp } from "./pages/Authentication/signUp.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {SignIn} from "./pages/Authentication/signIn.jsx";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path={Paths.HOME} element={<Layout />}>
           <Route path={Paths.HOME} element={<Home />} />
+          <Route path={"/signup"} element={<SignUp />} />
+          <Route path={"/signin"} element={<SignIn />} />
         </Route>
       </Routes>
     </Router>
---
+++ packages/client/src/pages/Authentication/signIn.jsx
@@ -0,0 +1,77 @@
+import {Box, Button, Container, Grid, Link, TextField, Typography} from "@mui/material";
+
+export const SignIn = () => {
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			email: data.get('email'),
+			password: data.get('password'),
+		});
+
+	};
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Typography component="h1" variant="h4">
+					Sign In
+				</Typography>
+
+				<Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+							/>
+						</Grid>
+
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="new-password"
+							/>
+						</Grid>
+					</Grid>
+
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Sign In
+					</Button>
+
+					<Grid container justifyContent="flex-end">
+						<Grid item>
+							New to the ClassGuardian?
+							<Link href="/signup" variant="body1">
+								Sign Up Here
+							</Link>
+						</Grid>
+					</Grid>
+
+				</Box>
+			</Box>
+		</Container>
+	);
+};
