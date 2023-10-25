5bfd39997cbd46d8a0e7163f8b08d0c4dcc19968
Yinkai <tonyxiong995@gmail.com>
2023-10-25 17:17:58 -0400

    Fix missing } bracket.

--- packages/client/src/pages/Home/home.jsx
+++ packages/client/src/pages/Home/home.jsx
@@ -11,13 +11,13 @@ export const Home = () => {
   const [addDialogOpen, setAddDialogOpen] = useState(false);
   const [studentAddOpen, setStudentAddOpen] = useState(false);
   const [user, setUser] = useState({});
-  const { token, decoded_token } = useAuth();
+  const {token, decoded_token} = useAuth();
   const fullName = `${user.firstName} ${user.lastName}`;
   const [courses, dispatch] = useReducer(courseReducer, [], () => []);
 
   useEffect(() => {
     const getCourses = async () => {
-      if (decoded_token) {
+      if(decoded_token) {
         const res = await axiosInstance.get(`/user/${decoded_token.id}`, {
           headers: {
             Authorization: `Bearer ${token}`
@@ -25,7 +25,7 @@ export const Home = () => {
         });
 
         setUser(res.data);
-        dispatch({ type: Actions.SET_COURSES, payload: res.data.courses });
+        dispatch({type: Actions.SET_COURSES, payload: res.data.courses});
       }
     };
 
@@ -33,69 +33,72 @@ export const Home = () => {
   }, [decoded_token]);
 
   const handleOpen = () => {
-    if (user.role === 1) {
+    if(user.role === 1) {
       handleAddDialogOpen();
     } else {
       handleStudentAddOpen();
     }
+  }
 
-  const addCourse = (course) => {
-    dispatch({ type: Actions.ADD_COURSE, payload: course });
-  };
+    const addCourse = (course) => {
+      dispatch({type: Actions.ADD_COURSE, payload: course});
+    };
 
-  const deleteCourse = (courseId) => {
-    dispatch({ type: Actions.DELETE_COURSE, payload: courseId });
-  };
+    const deleteCourse = (courseId) => {
+      dispatch({type: Actions.DELETE_COURSE, payload: courseId});
+    };
 
-  const handleAddDialogOpen = () => {
-    setAddDialogOpen(true);
-  };
+    const handleAddDialogOpen = () => {
+      setAddDialogOpen(true);
+    };
 
-  const handleAddDialogClose = () => {
-    setAddDialogOpen(false);
-  };
+    const handleAddDialogClose = () => {
+      setAddDialogOpen(false);
+    };
 
-  const handleStudentAddOpen = () => {
-    setStudentAddOpen(true);
-  };
+    const handleStudentAddOpen = () => {
+      setStudentAddOpen(true);
+    };
 
-  const handleStudentAddClose = () => {
-    setStudentAddOpen(false);
-  };
+    const handleStudentAddClose = () => {
+      setStudentAddOpen(false);
+    };
 
-  return (
-    <Stack>
-      <Stack direction="row" alignItems="center" justifyContent="space-between">
-        <Typography
-          component="h1"
-          variant="h3"
-          sx={{
-            color: '#265792'
-          }}
-        >
-          {fullName}
-        </Typography>
-        <Box>
-          <Button
-            variant="contained"
+    return (
+      <Stack>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <Typography
+            component="h1"
+            variant="h3"
             sx={{
-              background: '#265792',
-              color: 'white',
-              fontWeight: 'bold'
+              color: '#265792'
             }}
-            onClick={handleAddDialogOpen}
           >
-            Add Course
-          </Button>
-          <AddCourseDialog open={addDialogOpen} onClose={handleAddDialogClose} addCourse={addCourse} />
-        </Box>
+            {fullName}
+          </Typography>
+          <Box>
+            <Button
+              variant="contained"
+              sx={{
+                background: '#265792',
+                color: 'white',
+                fontWeight: 'bold'
+              }}
+              onClick={handleOpen}
+            >
+              Add Course
+            </Button>
+            <AddCourseDialog open={addDialogOpen} onClose={handleAddDialogClose} addCourse={addCourse}/>
+            <AddStudentCourseDialog open={studentAddOpen} onClose={handleStudentAddClose} />
+          </Box>
+        </Stack>
+        <Stack flexWrap="wrap" direction="row" gap={5} sx={{mt: 7}}>
+          {courses &&
+            courses.map((course) => {
+              return <CourseCard key={course.courseId} courseName={course?.Course?.name} courseId={course.courseId}
+                                 deleteCourse={deleteCourse}/>;
+            })}
+        </Stack>
       </Stack>
-      <Stack flexWrap="wrap" direction="row" gap={5} sx={{ mt: 7 }}>
-        {courses &&
-          courses.map((course) => {
-            return <CourseCard key={course.courseId} courseName={course?.Course?.name} courseId={course.courseId} deleteCourse={deleteCourse} />;
-          })}
-      </Stack>
-    </Stack>
-  );
-};
+    );
+  };
