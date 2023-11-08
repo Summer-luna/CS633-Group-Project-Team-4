870aa078f3a1337d4cc9f5b5086366fdd4112858
Xinyue (Luna) <xinyuec57@gmail.com>
2023-11-08 13:24:14 -0500

    feat: add user fake data

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
+
 async function main() {
   // insert Semester
   const semesterMockData = [
@@ -16,6 +18,76 @@ async function main() {
       name: 'summer'
     }
   ];
+
+  const users = [
+    {
+      email: 'cxyue@bu.edu',
+      password: '12345',
+      lastName: 'Chen',
+      firstName: 'Xinyue',
+      buID: '1234567',
+      role: 0
+    },
+    {
+      email: 'ykxiong@bu.edu',
+      password: '12345',
+      lastName: 'Xiong',
+      firstName: 'Yinkai',
+      buID: '12345678',
+      role: 0
+    },
+    {
+      email: 'nelsonmo@bu.edu',
+      password: '12345',
+      lastName: 'Montesinos',
+      firstName: 'Nelson',
+      buID: '123456789',
+      role: 0
+    },
+    {
+      email: 'lyl1021@bu.edu',
+      password: '12345',
+      lastName: 'Li',
+      firstName: 'Yilin',
+      buID: '123456787',
+      role: 0
+    },
+    {
+      email: 'elentukh@bu.edu',
+      password: '12345',
+      lastName: 'Elentukh',
+      firstName: 'Alex',
+      buID: '1234567877',
+      role: 1
+    }
+  ];
+
+  // insert Users
+  users.map(async (newUser) => {
+    const SALT_ROUNDS = 15;
+    const pwdHash = newUser.password ? await bcrypt.hash(newUser.password, SALT_ROUNDS) : null;
+
+    await prisma.user.upsert({
+      where: {
+        email: newUser.email
+      },
+      create: {
+        email: newUser.email,
+        lastName: newUser.lastName,
+        firstName: newUser.firstName,
+        buID: newUser.buID,
+        role: newUser.role,
+        password: pwdHash
+      },
+      update: {
+        lastName: newUser.lastName,
+        firstName: newUser.firstName,
+        buID: newUser.buID,
+        role: newUser.role,
+        password: pwdHash
+      }
+    });
+  });
 
   semesterMockData.map(async (semester) => {
     await prisma.semester.upsert({
