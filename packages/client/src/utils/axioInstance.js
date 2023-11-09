629ab47e68673e69bef710724e6e7141a5fce2d7
Xinyue (Luna) <xinyuec57@gmail.com>
2023-11-08 23:12:38 -0500

    fix: remove http timeout

--- packages/client/src/utils/axioInstance.js
+++ packages/client/src/utils/axioInstance.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
-  timeout: 3000
+  baseURL: import.meta.env.VITE_BACKEND_URL
 });
